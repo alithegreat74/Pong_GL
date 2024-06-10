9c8d70487a3f20ef76f3408280d2b84963d9aacb
Alireza Mohammadi <86660759+alithegreat74@users.noreply.github.com>
2024-06-10 15:34:51 +0330

    initial project

---
+++ main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+int main()
+{
+	if (!glfwInit()) {
+		std::cout << "Failed to Initialize glfw\n";
+		return -1;
+	}
+
+
+	return 0;
+}
