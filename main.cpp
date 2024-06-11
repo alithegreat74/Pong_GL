5cc5b16347db000dc584dc04f873e5fbbe142cf9
Alireza Mohammadi <86660759+alithegreat74@users.noreply.github.com>
2024-06-11 06:53:30 +0330

    added the third party libraries

--- main.cpp
+++ main.cpp
@@ -1,14 +1,6 @@
 ﻿#include <iostream>
-#include "GL/glew.h"
-#include "GLFW/glfw3.h"
-
-int main()
-{
-	if (!glfwInit()) {
-		std::cout << "Failed to Initialize glfw\n";
-		return -1;
-	}
 
 
-	return 0;
+int main() {
+
 }
